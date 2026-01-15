e3ea375657081a897ae1a88ea7bfb64f135037f1
ria <ria.bains@yahoo.com>
2026-01-15 13:42:32 +0000

    feat: Changed outline focus colour to pink

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -224,7 +224,7 @@ private const val ERROR_404_HTML =
                 text-decoration: underline;
             }
             a:focus {
-                outline: 3px solid #4A90E2;
+                outline: 3px solid #e24abc;
                 outline-offset: 2px;
             }
         </style>
